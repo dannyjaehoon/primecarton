0b9f2d35d520d622867ef459ae8ca23dfdd1fd1e
jaehoonlee <jaelee9212>
2025-11-19 19:54:40 -0500

    fix error

--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";  
-import { PrismaClient } from "@/lib/generated/prisma/client"; 
+import { PrismaClient } from "@prisma/client"; 
 import sampleData from "./sample-data";
 
 
